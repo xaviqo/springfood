62635171e5f56a4cd24b7600929c7475ce2c4519
xaviqo <silent90s@gmail.com>
2022-03-24 11:12:20 +0100

    Second Deploy

--- target/classes/static/assets/js/register/register.js
+++ target/classes/static/assets/js/register/register.js
@@ -1,8 +1,11 @@
 async function createAccount(){
 
+    let customerInfo = {};
+
     const request = await fetch('api/order/getProductCart/' + id, {
         method: 'GET',
-        headers: getHeaders()
+        headers: getHeaders(),
+        body: JSON.stringify(customerInfo)
     });
 
     const productJSON = await request.json();
